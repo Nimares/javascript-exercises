eb9f06529b659c0ace6b6a124dd666a4ca857153
Nimares <nikolai.resvold@gmail.com>
2024-03-19 17:13:50 +0100

    feat: evaluates if year is leapyear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,30 @@
-const leapYears = function() {
-
+const leapYears = function(yearVar) {
+    if (yearVar%4 != 0) 
+        return false
+    else if (
+        yearVar%4 == 0 &&
+        yearVar%100 == 0 &&
+        yearVar%400 != 0
+        )
+        return false 
+    else 
+        return true
+        
 };
+
+console.log(leapYears(1900))
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// Must be: Divisible by 4
+// if divisible by 100, must also be divisible by 400
+
+// yearVar%4 != 0 false
+// yearVar%4 == 0 && yearVar%100 == 0 && yearVar%400 != 0 false
+
+
+// yearVar%100 != 0 true
+// yearVar%100 == 0 && yearVar%400 == 0 true
+
+
