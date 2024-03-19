d1a8d4e409f9c1727d66687e1777a3f15afa521d
Nimares <nikolai.resvold@gmail.com>
2024-03-19 13:57:36 +0100

    feat(reverseString) reverses string input

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString ="";
+    let stringNegativeIndex = -(string.length);
 
+    for (i=-1; i>=stringNegativeIndex ;i--) {
+        reversedString = reversedString+string.at(i);
+    }
+    return reversedString;
 };
+
+reverseString("Hello");
+
+//Can be completed using slice() or at()
 
 // Do not edit below this line
 module.exports = reverseString;
