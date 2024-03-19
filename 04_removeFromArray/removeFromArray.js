e300cfabd7eecfe579e98fa73e193e3662adfd3f
Nimares <nikolai.resvold@gmail.com>
2024-03-19 14:16:05 +0100

    feat: removes targetVal from input arr

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, targetVal) {
+    const targetValIndex = arr.indexOf(targetVal);
+    arr.splice(targetValIndex, 1);
+    return arr
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
