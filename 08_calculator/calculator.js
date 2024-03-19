00c951ed618cf10643036c0b59d585252503721e
Nimares <nikolai.resvold@gmail.com>
2024-03-19 17:56:08 +0100

    feat: add, substract, sum funct

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,12 +1,21 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  const sum = (+num1 + +num2);
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	const sum = (+num1 - +num2)
+  return sum;
 };
 
-const sum = function() {
+const sum = function(arr) {
+  let sum = 0;
+  let arrIndex = arr.length-1;
+  if (arr.length == 0) return 0
+  for (i=0; i<=arrIndex; i++) {
+    sum += arr[i];
+  }
+  return sum
 	
 };
 
