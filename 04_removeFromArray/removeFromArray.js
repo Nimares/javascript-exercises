0bb3aaa95947c0bf88e18f47f953568d3d58ff7b
Nimares <nikolai.resvold@gmail.com>
2024-03-19 14:32:22 +0100

    fix(removeFromArray): accepts multiple targetVal args

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,11 @@
-const removeFromArray = function(arr, targetVal) {
-    const targetValIndex = arr.indexOf(targetVal);
-    arr.splice(targetValIndex, 1);
+const removeFromArray = function(arr, ...targetVals) {
+    for (let targetVal of targetVals) {
+        const targetValIndex = arr.indexOf(targetVal);
+        arr.splice(targetValIndex, 1);
+    }
     return arr
 };
 
-removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 4);
 // Do not edit below this line
 module.exports = removeFromArray;
