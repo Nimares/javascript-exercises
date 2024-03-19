7849b53e22057565d33520f0977b2b1cc18e4f2e
Nimares <nikolai.resvold@gmail.com>
2024-03-19 15:23:27 +0100

    refactor: final version

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,11 +3,10 @@ const removeFromArray = function(arr, ...targetVals) {
         while (arr.includes(targetVal)){
             const targetValIndex = arr.indexOf(targetVal);
             arr.splice(targetValIndex, 1);
-        } ;
+        }
     }
     return arr
 };
 
-console.log(removeFromArray([3, 1, 2, 3, 4], 3, 4));
 // Do not edit below this line
 module.exports = removeFromArray;
