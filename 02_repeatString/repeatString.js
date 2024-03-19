7252485e0ccda4e95c282ba0a3e43da5c60bfaae
Nimares <nikolai.resvold@gmail.com>
2024-03-19 13:08:22 +0100

    fix(repeatString): allows for input 1-10

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,11 @@
 const repeatString = function(string, num) {
-    let strResult = string;
-    for (i=1; i<num; i++) {
+    let strResult = "";
+    for (i=1; i<=num; i++) {
         strResult += string;
     }
     return strResult
 };
 
-console.log(repeatString("hey", 3));
+
 // Do not edit below this line
 module.exports = repeatString;
