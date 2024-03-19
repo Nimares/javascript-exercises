f379c524b2bfafbb12e4eea8aca2366b3e062173
Nimares <nikolai.resvold@gmail.com>
2024-03-19 17:32:42 +0100

    feat: converts to celsius or fahrenheit, with 1 decimal specificity

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  const degCelsius = ((deg - 32)*(5/9));
+  return +degCelsius.toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  const degFahrenheit = (deg * (9/5) + 32);
+  return +degFahrenheit.toFixed(1)
 };
 
 // Do not edit below this line
